1c0eb8b456d9025cd99ad4fb6cd26bb3a61797ce
lorenzo <lrnzmd@gmail.com>
2019-10-21 12:11:19 +0200

    collegamenti layout

---
+++ app/src/main/java/com/example/diarioscuola/Compiti.kt
@@ -0,0 +1,12 @@
+package com.example.diarioscuola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Compiti : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compiti)
+    }
+}
