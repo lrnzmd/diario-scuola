babfc8d8aa455551512f4286c4904c34f5ad9717
lorenzo <lrnzmd@gmail.com>
2019-10-21 12:43:17 +0200

    creazione menu

--- app/src/main/java/com/example/diarioscuola/MainActivity.kt
+++ app/src/main/java/com/example/diarioscuola/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.diarioscuola
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 
@@ -55,4 +59,23 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+
+        return when (item.itemId) {
+            R.id.button_compiti -> {
+                val intent = Intent(this, Compiti::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
