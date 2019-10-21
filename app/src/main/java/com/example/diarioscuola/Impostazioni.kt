d7d1736e87a7d07d65f69d7f35db25b5fcb27880
lorenzo <lrnzmd@gmail.com>
2019-10-21 12:26:22 +0200

    layout vari

---
+++ app/src/main/java/com/example/diarioscuola/Impostazioni.kt
@@ -0,0 +1,12 @@
+package com.example.diarioscuola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Impostazioni : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_impostazioni)
+    }
+}
